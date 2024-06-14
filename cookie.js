deec84e72f1474b7a8757bb836d7db7c25c021d1
Zaqueu Alves <69696525+zaqueu-git@users.noreply.github.com>
2024-06-14 20:24:19 -0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,29 +1,29 @@
-class Cookie {
+const cookies = {
+    add(name, value) {
+        var d = new Date();
+        d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    },
+    remove(name) {
+        document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    },
+    get(name) {
+        var name = name + "=";
+        var ca = document.cookie.split(';');
 
-  add(name, value) {
-    var d = new Date();
-    d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  }    
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            
+            while (c.charAt(0) == ' ') {
+               c = c.substring(1);
+            }
 
-  remove(name) {
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  }
+            if (c.indexOf(name) == 0) {
+               return c.substring(name.length, c.length);
+            }
+        }
 
-  get(name) {
-    var name = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+        return ""; 
     }
-    return ""; 
-  }
-
-}
+};
